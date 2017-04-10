d041a5fac3a15c4b78afb0fa25ca3bf15af363a6
Andrew Chan <accsuperstar@gmail.com>
2017-04-10 17:16:16 -0500

    end of the day commit. fixed bugs

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -17,7 +17,7 @@ router.get('/nurse', function(req, res, next) {
   res.render('nurse');
 });
 
-router.get('/student', function(req, res, next) {
+router.get('/guardian', function(req, res, next) {
   res.render('guardian');
 });
 
@@ -43,7 +43,5 @@ router.get('/callback',
     console.log('req4: ', req);
     console.log('res4: ', res);
   });
-
-
 
 module.exports = router;
