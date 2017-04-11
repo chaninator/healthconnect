d77a734f6ac2b0016bcd715be4d84eb38b0c2fb9
Andrew Chan <accsuperstar@gmail.com>
2017-04-11 14:25:19 -0500

    nurse modals complete

--- backend/public/javascripts/script.js
+++ backend/public/javascripts/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-
+  // Hide and show nurse modals for "generate report"
   $('#modal-report').modal('show');
   $('.btn-report-next').on('click', function(e) {
     e.preventDefault();
@@ -14,8 +14,21 @@ $(document).ready(function() {
     $('#modal-report2').modal('hide');
     $('#modal-report3').modal('show');
   });
+  $('.btn-symptoms-next').on('click', function(e) {
+    e.preventDefault();
+    /* Act on the event */
+    $('#modal-report3').modal('hide');
+    $('#modal-report4').modal('show');
+  });
+  $('.btn-notes-next').on('click', function(e) {
+    e.preventDefault();
+    /* Act on the event */
+    $('#modal-report4').modal('hide');
+    $('#modal-report5').modal('show');
+  });
 
-  btn-symptoms-next
+
+
 
 
 
