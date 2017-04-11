08db0fe06783d6a6d4c4f2d48fb18eebeec97f6a
Andrew Chan <accsuperstar@gmail.com>
2017-04-11 14:00:55 -0500

    completed 3 modals

--- backend/public/javascripts/script.js
+++ backend/public/javascripts/script.js
@@ -2,7 +2,20 @@ $(document).ready(function() {
 
 
   $('#modal-report').modal('show');
+  $('.btn-report-next').on('click', function(e) {
+    e.preventDefault();
+    /* Act on the event */
+    $('#modal-report').modal('hide');
+    $('#modal-report2').modal('show');
+  });
+  $('.btn-vitals-next').on('click', function(e) {
+    e.preventDefault();
+    /* Act on the event */
+    $('#modal-report2').modal('hide');
+    $('#modal-report3').modal('show');
+  });
 
+  btn-symptoms-next
 
 
 
