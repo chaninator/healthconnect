fe3da0ca11c94aadb05c452d4021bfa1e662b6bb
Samantha Keathley <samncollette@gmail.com>
2017-04-11 14:06:38 -0500

    changed app.js

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,4 @@
 require('dotenv').config();
-
 var express = require('express');
 var path = require('path');
 var favicon = require('serve-favicon');
@@ -13,23 +12,13 @@ var dotenv = require('dotenv');
 var passport = require('passport');
 var Auth0Strategy = require('passport-auth0');
 var mongoose = require('mongoose');
-var passport = require('passport');
-var Auth0Strategy = require('passport-auth0');
-
 dotenv.load();
-
 var routes = require('./routes/index');
 var user = require('./routes/user');
-// var guardians = require('./routes/guardians');
+var students = require('./routes/students');
 var nurses = require('./routes/nurses');
-
 var reports = require('./routes/reports');
 var doctors = require('./routes/doctors');
-
-
-var studentProfile = require('./routes/studentProfile');
-var createreport = require('./routes/createreport');
-
 
 // This will configure Passport to use Auth0
 var strategy = new Auth0Strategy({
@@ -43,26 +32,19 @@ var strategy = new Auth0Strategy({
     // profile has all the information from the user
     return done(null, profile);
   });
-
 passport.use(strategy);
-
 // you can use this section to keep a smaller payload
 passport.serializeUser(function(user, done) {
   done(null, user);
 });
-
 passport.deserializeUser(function(user, done) {
   done(null, user);
 });
-
 var app = express();
-
 mongoose.connect(process.env.HEALTH_DB);
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
@@ -77,25 +59,20 @@ app.use(session({
 app.use(passport.initialize());
 app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
-
 app.use('/', routes);
 app.use('/user', user);
-
 // app.use('/users', users);
-// app.use('/guardians', guardians);
-
+app.use('/students', students);
 app.use('/nurses', nurses);
-
-
+app.use('/reports', reports);
+app.use('/doctors', doctors);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
-
 // error handlers
-
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
@@ -107,7 +84,6 @@ if (app.get('env') === 'development') {
     });
   });
 }
-
 // production error handler
 // no stacktraces leaked to user
 app.use(function(err, req, res, next) {
@@ -117,6 +93,4 @@ app.use(function(err, req, res, next) {
     error: {}
   });
 });
-
-
 module.exports = app;
