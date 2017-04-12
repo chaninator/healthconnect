272a1bb45c4dffed1c62110b6921969bdb4c3fa5
Andrew Chan <accsuperstar@gmail.com>
2017-04-12 16:48:38 -0500

    added landing page

--- backend/public/javascripts/script.js
+++ backend/public/javascripts/script.js
@@ -8,21 +8,42 @@ $(document).ready(function() {
     $('#modal-report').modal('hide');
     $('#modal-report2').modal('show');
   });
+  $('.btn-date-next').on('click', function(e) {
+    e.preventDefault();
+    var date = $('#date-textarea').val();
+    console.log('date: ', date);
+    /* Act on the event */
+    if (!date) {
+      // alert('Please make sure to fill out text');
+      sweetAlert("Oops...", "Please fill out form!", "error");
+    } else {
+      $('#modal-report2').modal('hide');
+      $('#modal-report3').modal('show');
+    }
+  });
   $('.btn-vitals-next').on('click', function(e) {
     e.preventDefault();
     var vitals = $('#vitals-textarea').val();
     console.log('vitals: ', vitals);
     /* Act on the event */
-    $('#modal-report2').modal('hide');
-    $('#modal-report3').modal('show');
+    if (!vitals) {
+      alert('Please make sure to fill out text');
+    } else {
+      $('#modal-report3').modal('hide');
+      $('#modal-report4').modal('show');
+    }
   });
   $('.btn-symptoms-next').on('click', function(e) {
     e.preventDefault();
     var symptoms = $('#symptoms-textarea').val();
     console.log('symptoms: ', symptoms);
     /* Act on the event */
-    $('#modal-report3').modal('hide');
-    $('#modal-report4').modal('show');
+    if (!symptoms) {
+      alert('Please make sure to fill out text');
+    } else {
+      $('#modal-report4').modal('hide');
+      $('#modal-report5').modal('show');
+    }
   });
   $('.btn-notes-next').on('click', function(e) {
     e.preventDefault();
@@ -30,9 +51,12 @@ $(document).ready(function() {
     console.log('notes: ', notes);
 
     /* Act on the event */
-    $('#modal-report4').modal('hide');
-    $('#modal-report5').modal('show');
+    if (!notes) {
+      alert('Please make sure to fill out text');
+    } else {
+      $('#modal-report5').modal('hide');
+      $('#modal-report6').modal('show');
+    }
   });
-
 
 });
