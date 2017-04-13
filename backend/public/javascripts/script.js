a57362ca63e483534042ff6db3d5e5019f70331d
Samantha Keathley <samncollette@gmail.com>
2017-04-12 22:03:57 -0500

    began dropdown menu

--- backend/public/javascripts/script.js
+++ backend/public/javascripts/script.js
@@ -61,4 +61,12 @@ $(document).ready(function() {
       $('#modal-report6').modal('show');
     }
   });
+  $('#guardDrop').on('click', function(e){
+    e.preventDefault();
+    if ($('.dropMenu').hasClass('hidden')){
+        $('.dropMenu').show();
+    } else{
+      $('.dropMenu').hide();
+    }
+  });
 });
